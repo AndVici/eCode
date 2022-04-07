b967a31a3dea3487aea84678d9f9f7209be72d81
Imara Willie Johnson <3279904+AndVici@users.noreply.github.com>
2022-04-07 10:59:42 -0400

    Add files via upload

---
+++ Exercise1.py
@@ -0,0 +1,23 @@
+def main():
+
+    #Create a variable named x, and assign the value 50 to it
+
+    
+
+    y = 7
+    
+    #print the sum of x and y
+
+    
+
+    animals = ["Koala", "Cow", "Octopus", "Wolf", "Centipede", "Rabbit"]
+
+    #print the 3rd item in animals, "Octopus"
+
+    pet = {
+        "name" : "spot",
+        "species" : "dog",
+        "age" : 4
+        }
+
+    #print the value of species
